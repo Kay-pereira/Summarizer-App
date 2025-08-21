c42d694b27023ab4108973d16efdad71d1be1963
Kay-pereira <edmundpereira18@gmail.com>
2025-08-21 21:49:58 +0000

    updated allowed hosts

--- summarizer/core/settings.py
+++ summarizer/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DEBUG', default = False, cast = bool)
 
 
-ALLOWED_HOSTS = [ '127.0.0.1','localhost']
+ALLOWED_HOSTS = [ '127.0.0.1','http://localhost:5173']
 
 
 # Application definition
