6171e4940fd9356262e19719fb5a79e9bc8d0b8d
Kay-pereira <edmundpereira18@gmail.com>
2025-08-23 14:07:20 +0000

    added whitenoise for static files config

--- summarizer/core/settings.py
+++ summarizer/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -140,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = False # For development only! ⚠️
 # Optional safer version:
-CORS_ALLOWED_ORIGINS = [ "https://summarizer-app-sepia.vercel.app" ]
+CORS_ALLOWED_ORIGINS = [ "https://summarizer-app-sepia.vercel.app", "http://localhost:5173" ]
 
 REST_FRAMEWORK = {
      'DEFAULT_AUTHENTICATION_CLASSES': (
