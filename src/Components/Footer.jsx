0df2d1c0f9fd0dc7567f335fd0874da153ebf2aa
Kay-pereira <edmundpereira18@gmail.com>
2025-08-25 13:03:48 +0000

    updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer(){
         <div className="footer-card">
             <p>Made by Edmund K. Pereira</p>
             <p>NOTE: Your activity may be used to improve this service</p>
-            <p>Use default sign up if concerned about sharing a password: username: user pass: 12345678</p>
+            <p>Use default sign up if concerned about sharing your info username: user pass: 12345678</p>
             <p>For info or colab: edmundpereira18@gmail.com and Kay-pereira on github</p>
         </div>
         </>
