21a13beb3c6d3372bfe6109b7d2a6116fceb8b9d
Kay-pereira <edmundpereira18@gmail.com>
2025-08-25 12:19:15 +0000

    added admin to url and made migrations

--- summarizer/core/urls.py
+++ summarizer/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from summarizer.views import RegisterView, SummarizeView, SummaryListView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # auth
+    path('admin/', admin.site.urls),
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
